f7e36d771f3ec1b194e37457d1cd0b38e6d60d63
Alessandro Silva <alessandro-almeida-silva@hotmail.com>
2025-02-12 17:38:05 -0300

    Feat - DbContext Fix - Clientes

---
+++ Data/DbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Gen_Sim.Data
+{
+    public class DbContext
+    {
+        public static string ConnectionString { get { return "Data Source=db\\db.db"; } }
+        
+        public DbContext()
+        {
+
+        }
+
+        private void Start()
+        {
+            if (!File.Exists("db\\db.db"))
+                CreateDataBase();
+        }
+
+        private void CreateDataBase()
+        {
+            using(var connection = new SqliteConnection(ConnectionString))
+            {
+                connection.Open();
+                string sqlTables = @"
+                        CREATE TABLE Clientes 
+                        (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Nome VARCHAR(100) NOT NULL,
+                            CnpjCpf VARCHAR(14) UNIQUE NOT NULL,
+                            InscricaoEstadual VARCHAR(9),
+                            Cep VARCHAR(8),
+                            Logradouro VARCHAR(100),
+                            Bairro VARCHAR(50),
+                            Numero INTEGER DEFAULT 0 CHECK (VALOR >= 0),
+                            Estado VARCHAR(50) NOT NULL,
+                            Telefone VARCHAR(11),
+                            Whatssap VARCHAR(11),
+                            Email VARCHAR(100)
+                        );";
+            }
+        }
+    }
+}
--- Model/Clientes.cs
+++ Model/Clientes.cs
@@ -9,6 +9,7 @@ namespace Gen_Sim.Model
     public class Clientes
     {
         public int Id { get; set; }
+        public string Nome { get; set; }
         public string CnpjCpf { get; set; }
         public string InscricaoEstadual { get; set; }
         public string Cep { get; set; }
@@ -16,7 +17,6 @@ namespace Gen_Sim.Model
         public string Bairro { get; set; }
         public int Numero { get; set; }
         public string Estado { get; set; }
-        public string Uf { get; set; }
         public string Telefone { get; set; }
         public string Whatssap { get; set; }
         public string Email { get; set; }
